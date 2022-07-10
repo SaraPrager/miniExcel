b0155d7be88cbb8721ecdea25cc8608de46350ab
Sara Prager <sari.prager@gmail.com>
2022-07-10 22:27:40 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# miniExcel
+# Mini Excel
+Mini excel utilities
+### How to run?
+```
+npm run mini-excel <input file path>
+```
+### How to use?
+Press `a` to print the current state
+
+
+Press `b <index> <value>` to change a value
---
+++ mini-excel.js
@@ -0,0 +1,38 @@
+import { promises as fs } from 'fs';
+import { evaluate } from 'mathjs';
+
+export class MiniExcel {
+    _cellMap = {};
+    constructor() {}
+
+    async initCells(filePath) {
+        try {
+            const fileInput = await fs.readFile(filePath, { encoding: 'utf8' });
+            fileInput.split(', ').forEach((value, index) => this._cellMap[index] = value);
+        } catch (e) {
+            console.error('Invalid input');
+        }
+    }
+
+    setValue(cellIndex, cellValue) {
+        this._cellMap[cellIndex] = cellValue;
+        console.log(`Cell #${cellIndex} changed to ${cellValue}`);
+    }
+
+    getState() {
+        return Object.keys(this._cellMap).map(cellIndex => 
+            `[${cellIndex}: ${this.getActualCellValue(cellIndex)}]`).join(', ');
+    }
+
+    getActualCellValue(cellIndex) {
+        let cellValue = this._cellMap[cellIndex];
+
+        if (!cellValue?.startsWith('=')) {
+            return cellValue;
+        }
+
+        cellValue = cellValue.replace('=', '')
+            .replace(/({)(\d)(})/, (_match, _$1, $2) => { return this.getActualCellValue($2) });
+        return evaluate(cellValue);
+    }
+}
